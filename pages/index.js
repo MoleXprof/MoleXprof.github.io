e7900edbb8be7ad9ee43efc3a2c4cee6df31dde4
MoleXprof <MoleXprof@users.noreply.github.com>
2023-01-14 20:00:18 +0000

    deploy: ae73b4f45b5b92c792be3dfde54ae88d9cf61e4a

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from "../components/layouts/index";
-
-const Index = () => {
-  return <Home />;
-};
-
-export default Index;
--- utils/index.ts
+++
@@ -1,3 +0,0 @@
-export const classnames = (...classes: any[]) => {
-  return classes.filter(Boolean).join(" ");
-};
