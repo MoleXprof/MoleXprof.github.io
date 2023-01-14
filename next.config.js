602d188c6a587eec27f84227d38b49a9bdb80799
Kyle <kkhc99@gmail.com>
2023-01-14 18:11:16 -0500

    revert changes back

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  assetPrefix: isProd ? '/MoleXprof.github.io/' : ' ',
-  images: {
-    unoptimized: true,
-  }
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
